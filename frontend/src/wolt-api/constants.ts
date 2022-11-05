6426c34f57a46988721cde07126ffbac03f6b722
Iron Tony <lifegamecom1@gmail.com>
2022-11-06 00:15:36 +0200

    Added requestDelivery Wolt API function

--- frontend/src/wolt-api/constants.ts
+++ frontend/src/wolt-api/constants.ts
@@ -2,7 +2,7 @@
 // TODO move secrets to the backend
 // TODO fix CORS
 
-export const cors_fix = 'https://cors-anywhere.herokuapp.com'
+export const cors_fix = 'http://localhost:8080'
 export const wolt_host = 'https://daas-public-api.development.dev.woltapi.com/merchants'
 export const wolt_merchant_id = '6364dffc8018ce361efafc73'
 export const wolt_token = 'I5rKYVPYRxB9GHNR5V51gIH8QEy3mgCY6BueroKJiDM'
